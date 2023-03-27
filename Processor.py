8d4872bf99500a4970f67d4064bae074cf72ab66
zaryab bhatti <zaryabgbhatti@gmail.com>
2023-03-26 20:40:06 -0400

    refactoring file location and fixed largest transaction, single transaction logic

--- Processor.py
+++ Processor.py
@@ -3,9 +3,9 @@ sys.path.append('C:/Repos/BTC-Project/BTC-Project/packages/')
 
 from packages import requests
 import json
-import BTCAddress
+import DTO.BTCAddress as BTCAddress
 
-def get_addy_data(bitcoin_address):
+def get_address_data(bitcoin_address):
 	bitcoin_address = "bc1q3sn68x4qegp5776narh78rlh0y385mw8a3dnfq"
 	response = requests.get(f"https://blockchain.info/rawaddr/{bitcoin_address}")
 
@@ -19,24 +19,25 @@ def get_addy_data(bitcoin_address):
 def get_largest_sent_transaction_by_address(bitcoin_address):
 
 	# ONLY GIVES LARGEST SENT TRANSACTION BASED ON LAST 50 TRANSACTIONS
-	address = get_addy_data(bitcoin_address)
+	address = get_address_data(bitcoin_address)
 	maxSpent = -1
 	list_of_transactions = address.transactions
 	hasBatchTransactions = contains_batch_transactions(list_of_transactions)
 
 	if hasBatchTransactions:
-
+		print('********Batch transaction********')
 		for transaction in address.transactions:
 			for inpt in transaction["inputs"]:
 				if inpt["prev_out"]["spent"] == True:
 					maxSpent = max(maxSpent, inpt["prev_out"]["value"])
 			
 	else:
-
+		print('********Single transaction********')
 		for transaction in address.transactions:
 			print(transaction["inputs"])
-			if transaction["inputs"][0].prev_out.spent == True:
-				maxSpent = max(maxSpent, transaction.inputs.prev_out.value)
+			if transaction["inputs"][0]["prev_out"]["spent"] == True:
+				transactionVal = int(transaction["inputs"][0]["prev_out"]["value"])
+				maxSpent = max(maxSpent, transactionVal )
 
 				
 	return maxSpent
@@ -58,17 +59,8 @@ def contains_batch_transactions(transaction_list):
 	return False
 
 #x = get_addy_data("gggg")
-y = get_largest_sent_transaction_by_address("bc1q3sn68x4qegp5776narh78rlh0y385mw8a3dnfq")
+#y = get_largest_sent_transaction_by_address("bc1q3sn68x4qegp5776narh78rlh0y385mw8a3dnfq")
 
-#print(x)
-print(y)
 
-#y = 1891...
-#print(x.address)
-#print(x.total_BTC_received)
-#rint(x.total_BTC_sent)
-#print(x.final_balance)
 
-#inp = input("What btc address would you like to check?")
-#x = get_addy_data(inp)
 
